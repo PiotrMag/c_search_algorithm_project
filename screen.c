bfc920a70530a01da989326871e69b933d17b4a3
PiotrMag <piotr_mag@wp.pl>
2019-12-18 22:19:43 +0100

    Added some functions
    Added functions in screen.c for updating pixel and for deleting the SCREEN instance

--- screen.c
+++ screen.c
@@ -2,12 +2,15 @@
 
 extern struct Pixel
 {
+    char text;
     int pos_x, pos_y;
     int font_color, background_color;
 };
 
 struct Screen
 {
+    int width;
+    int height;
     struct Pixel ** pixels;
 };
 
@@ -23,6 +26,31 @@ SCREEN CreateScreen(int width, int height)
         pixels[i] = (struct Pixel*)malloc(width * sizeof(struct Pixel));
     }
     SCREEN screen = (SCREEN)malloc(sizeof(struct Screen));
+    screen->width = width;
+    screen->height = height;
     screen->pixels = pixels;
     return screen;
+}
+
+void UpdatePixelText(SCREEN scr, int x, int y, char new_text)
+{
+    scr->pixels[y][x].text = new_text;
+}
+
+void UpdatePixelColor(SCREEN scr, int x, int y, int new_font_color, int new_background_color)
+{
+    scr->pixels[y][x].font_color = new_font_color;
+    scr->pixels[y][x].background_color = new_background_color;
+}
+
+void DeleteScreen(SCREEN scr)
+{
+    int i = 0;
+    int screen_width = scr->width;
+    for (i = 0; i < screen_width; ++i)
+    {
+        free(scr->pixels[i]);
+    }
+    free(scr->pixels);
+    free(scr);
 }
--- screen.h
+++ screen.h
@@ -2,3 +2,6 @@ struct Screen;
 typedef struct Screen *SCREEN;
 
 SCREEN CreateScreen(int width, int height);
+void UpdatePixelText(SCREEN scr, int x, int y, char new_text);
+void UpdatePixelColor(SCREEN scr, int x, int y, int new_font_color, int new_background_color);
+void DeleteScreen(SCREEN scr);
