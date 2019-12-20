104e7c63c972fe13f49db949974018766ea45a86
PiotrMag <piotr_mag@wp.pl>
2019-12-20 22:29:56 +0100

    Added some code and fixed bug
    Added some code to check screen refreshing speed (partially) and while doing this fixed bug which prevented second set of drawing to take place (tried to read string into char).

--- another_test.c
+++ another_test.c
@@ -11,7 +11,7 @@ int main()
 {
     BUFFER buff = CreateBuffer(WIDTH, HEIGHT);
     int num = 97;
-    int background = 10;
+    int background = 8;
     int foreground = 0;
     int i, j;
     for (i = 0; i < HEIGHT; i++)
@@ -21,13 +21,32 @@ int main()
             PrintToBuffer(buff, j, i, num, background, foreground);
             background = ++background % 16;
             foreground = ++foreground % 16;
+            num++;
         }
     }
     DrawBuffer(buff);
-    DeleteBuffer(buff);
 
-    char x;
+    char x[10];
     scanf("%s", &x);
 
+    num = 97;
+    background = 0;
+    foreground = 8;
+    for (i = 0; i < HEIGHT; i++)
+    {
+        for (j = 0; j < WIDTH; j++)
+        {
+            PrintToBuffer(buff, j, i, num, background, foreground);
+            background = ++background % 16;
+            foreground = ++foreground % 16;
+            num++;
+        }
+    }
+    DrawBuffer(buff);
+
+    //char x;
+    scanf("%s", &x);
+
+    DeleteBuffer(buff);
     return 0;
 }
